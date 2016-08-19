e32289fa64aff0be2084d0d17df0c014b463d240
Ellina <dancalya@gmail.com>
2016-08-20 02:49:16 +0300

    created parent BaseTest.java moved buyIpone test tp separate BuyIPhone.java fixed switch to frame in buyIphone  test removed DataProviderClass.java

---
+++ src/test/java/calculator/BuyIPhone.java
@@ -0,0 +1,35 @@
+package calculator;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+import static org.hamcrest.Matchers.equalTo;
+
+public class BuyIPhone extends BaseTest {
+
+    @Rule
+    public ErrorCollector collector = new ErrorCollector();
+
+    @Test
+    public void buyIphone() throws Exception {
+        driver.get("http://city.com.ua/goods/smartphones/apple-iphone-6s-16gb-rose-gold.html");
+        driver.findElement(By.id("buy_credit_btn")).click();
+        WebElement frame = driver.findElement(By.xpath("//iframe[contains(@id,'fancybox-frame')]"));
+        driver.switchTo().frame(frame);
+        WebElement inputDownPayment = driver.findElement(By.xpath(".//div[@class='downpayment_block']/input"));
+        Actions builder = new Actions(driver);
+        builder.sendKeys(inputDownPayment, Keys.chord(Keys.CONTROL, "a"), "2000").perform();
+        inputDownPayment.sendKeys(Keys.ENTER);
+
+        int PUMB = Integer.parseInt(driver.findElement(By.xpath(".//tr[2]/td[3]")).getText().replace(" грн", ""));
+        int alfaBank = Integer.parseInt(driver.findElement(By.xpath(".//tr[3]/td[3]")).getText().replace(" грн", ""));
+        collector.checkThat(PUMB, equalTo(1519));
+        collector.checkThat(alfaBank, equalTo(1479));
+        driver.switchTo().defaultContent();
+    }
+}
--- src/test/java/calculator/Calculator.java
+++ src/test/java/calculator/Calculator.java
@@ -1,53 +1,46 @@
 package calculator;
 
-import org.junit.Rule;
-import org.junit.rules.ErrorCollector;
-import org.openqa.selenium.firefox.GeckoDriverService;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.*;
-import org.openqa.selenium.*;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
-import org.junit.runners.Parameterized;
-import org.junit.runner.RunWith;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.concurrent.TimeUnit;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.closeTo;
+import static org.hamcrest.Matchers.is;
+
 @RunWith(Parameterized.class)
-public class Calculator {
-    private static WebDriver driver;
-    private static String baseUrl;
+public class Calculator extends BaseTest {
 
     private String calcDate;
     private String state;
     private String grossPay;
     private String grossPayType;
-    private String payFrequency ;
+    private String payFrequency;
     private Boolean iAmExemptFrom;
     private double expectedNetPayResult;
 
     @Parameterized.Parameters
-    public  static Collection salaryData() {
+    public static Collection salaryData() {
         return Arrays.asList(
                 new Object[][]{
-                        { "12/31/2016", "Alaska", "1999", "Pay Per Period", "Weekly", false, 1422.37 },
-                        { "12/31/2017", "Hawaii", "75000", "Annually", "Annual", false, 49701.43}
+                        {"12/31/2016", "Alaska", "1999", "Pay Per Period", "Weekly", false, 1422.37},
+                        {"12/31/2017", "Hawaii", "75000", "Annually", "Annual", false, 49701.43}
                 }
         );
     }
 
-    public Calculator (String calcDate, String state, String grossPay, String grossPayType, String payFrequency, Boolean iAmExemptFrom, double expectedNetPayResult) {
-        this.calcDate=calcDate;
+    public Calculator(String calcDate, String state, String grossPay, String grossPayType, String payFrequency, Boolean iAmExemptFrom, double expectedNetPayResult) {
+        this.calcDate = calcDate;
         this.state = state;
         this.grossPay = grossPay;
         this.grossPayType = grossPayType;
@@ -56,68 +49,38 @@ public class Calculator {
         this.expectedNetPayResult = expectedNetPayResult;
     }
 
-    @BeforeClass
-    public static void setUp() throws Exception {
-        System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "\\src\\test\\resources\\geckodriver.exe");
-        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
-        capabilities.setCapability("marionette", true);
-        driver = new FirefoxDriver(capabilities);
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
-        baseUrl = "http://www.paycheckcity.com/";
-    }
 
     @Test
     public void testSalary() throws Exception {
-        driver.get(baseUrl + "calculator/salary/");
+        driver.get("http://www.paycheckcity.com/calculator/salary/");
         WebElement calcDateElement = driver.findElement(By.id("calcDate"));
         calcDateElement.clear();
         calcDateElement.sendKeys(calcDate);
-        driver.findElement(By.xpath(".//div[@id='widget_state']//input[@class='dijitReset dijitInputField dijitArrowButtonInner']")).click();
-        driver.findElement(By.xpath(".//div[@id='state_popup']//div[text()='"+state+"']")).click();
+        WebElement stateElement = driver.findElement(By.id("state"));
+        stateElement.clear();
+        stateElement.sendKeys(state);
         WebElement grossPayElement = driver.findElement(By.id("generalInformation.grossPayAmount"));
         grossPayElement.clear();
         grossPayElement.sendKeys(grossPay);
-        driver.findElement(By.xpath(".//div[@id='widget_generalInformation.grossPayMethodType']//input[@class=\"dijitReset dijitInputField dijitArrowButtonInner\"]")).click();
-        driver.findElement(By.xpath(".//div[@id=\"generalInformation.grossPayMethodType_popup\"]//div[text()='"+grossPayType+"']")).click();
-        driver.findElement(By.xpath(".//div[@id='widget_generalInformation.payFrequencyType']//input[@class=\"dijitReset dijitInputField dijitArrowButtonInner\"]")).click();
-        driver.findElement(By.xpath(".//div[@id=\"generalInformation.payFrequencyType_popup\"]//div[text()='"+payFrequency+"']")).click();
+        WebElement grossPayTypeElement = driver.findElement(By.id("generalInformation.grossPayMethodType"));
+        grossPayTypeElement.clear();
+        grossPayTypeElement.sendKeys(grossPayType);
+        WebElement payFrequencyElement = driver.findElement(By.id("generalInformation.payFrequencyType"));
+        payFrequencyElement.clear();
+        payFrequencyElement.sendKeys(payFrequency);
         Boolean valueOfCheckBox = driver.findElement(By.xpath(".//input[@name='generalInformation.exemptFederal']")).isSelected();
-        if (iAmExemptFrom != valueOfCheckBox)
-        { driver.findElement(By.xpath(".//input[@name='generalInformation.exemptFederal']")).click(); }
+        if (iAmExemptFrom != valueOfCheckBox) {
+            driver.findElement(By.xpath(".//input[@name='generalInformation.exemptFederal']")).click();
+        }
 
         driver.findElement(By.id("calculate")).click();
         Wait fluenWait = new FluentWait(driver)
                 .withTimeout(300, TimeUnit.SECONDS)
                 .pollingEvery(2, TimeUnit.SECONDS)
                 .ignoring(NoSuchElementException.class);
-        fluenWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//div[@id=\"dijit_TitlePane_1_pane\"]//span[@class=\"resultDef\" and text()=\"Net Pay\"]")));
-        double actualNetPayResult = Double.parseDouble(driver.findElement(By.xpath(".//div[@id=\"dijit_TitlePane_1_pane\"]//span[@class=\"resultData\"]")).getText().replace("$","").replace(",",""));
+        By netPayValue = By.xpath(".//div[@id=\"dijit_TitlePane_1_pane\"]//span[@class=\"resultData\"]");
+        fluenWait.until(ExpectedConditions.visibilityOfElementLocated(netPayValue));
+        double actualNetPayResult = Double.parseDouble(driver.findElement(netPayValue).getText().replace("$", "").replace(",", ""));
         assertThat(actualNetPayResult, is(closeTo(expectedNetPayResult, 5)));
-    }
-
-    @Rule
-    public ErrorCollector collector = new ErrorCollector();
-
-    @Test
-    public void buyIphone() throws Exception {
-        driver.get("http://city.com.ua/goods/smartphones/apple-iphone-6s-16gb-rose-gold.html");
-        driver.findElement(By.id("buy_credit_btn")).click();
-
-        (new WebDriverWait(driver, 1000))
-                .until(ExpectedConditions.presenceOfElementLocated(By.xpath(".//div[@class=\"fancy_title\"]")));
-        WebElement input = driver.findElement(By.className("downpayment r3"));
-        Actions builder = new Actions(driver);
-        builder.sendKeys(input, "2000").click().perform();
-
-        int PUMB= Integer.parseInt(driver.findElement(By.xpath(".//tr[2]/td[3]")).getText().replace(" грн", ""));
-        int alfaBank= Integer.parseInt(driver.findElement(By.xpath(".//tr[3]/td[3]")).getText().replace(" грн", ""));
-        collector.checkThat(PUMB, equalTo(1519));
-        collector.checkThat(alfaBank, equalTo(1479));
-    }
-
-    @AfterClass
-    public static void tearDown() throws Exception {
-        driver.quit();
     }
 }
